4de26230003d6eb8feb91fe48f612861af3cbbfe
dalviik <duglasapazaa@gmail.com>
2021-02-04 08:54:02 -0400

    Add services endpoint room, player

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ingresarRoom(idRoom: string) {
+    let url = URL_SERVICIOS + '/room/' + idRoom;
+    return this.http.get(url);
+  }
+
+  actualizarVoto(idPlayer: number, player: Object) {
+    let url = URL_SERVICIOS + '/room/' + idPlayer;
+    return this.http.post(url, player);
+  }
 }
+
--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../config/config';
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  crearRoom(room: Object) {
+    let url = URL_SERVICIOS + '/room';
+    return this.http.post(url, room);
+  }
+
+  obtenerPlayers(idRoom: string) {
+    let url = URL_SERVICIOS + '/room/' + idRoom;
+    return this.http.get(url);
+  }
+
+  actualizarPregunta(idRoom: string, room: Object) {
+    let url = URL_SERVICIOS + '/room/' + idRoom;
+    return this.http.put(url, room);
+  }
+
 }
