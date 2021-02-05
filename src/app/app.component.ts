be4e29e3fea946280659d642da6f4805835bd54c
dalviik <duglasapazaa@gmail.com>
2021-02-05 12:07:25 -0400

    Add audio files Add buttons play pause

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-poker-scrum';
+  @ViewChild('myAudio') myAudio: ElementRef;
+  title = 'pokerScrum';
+  statusAudio = true;
+  mute() {
+    this.myAudio.nativeElement.pause();
+    this.statusAudio = false;
+  }
+  unmute() {
+    this.myAudio.nativeElement.play();
+    this.statusAudio = true;
+  }
 }
